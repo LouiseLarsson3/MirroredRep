78e33cd7dc2afa9960304fc743c1171876bd610c
Louise <louise_larsson_s@hotmail.com>
2023-03-02 15:35:00 +0100

    New file

---
+++ calculate.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () 
+{
+int A, B, sum = 0;
+
+printf("Enter two numbers: \n");
+scanf("%d%d, &A, &B);
+
+sum = A + B;
+
+printf("Sum of the two numbers is: %d", sum);
+
+return 0;
+}
